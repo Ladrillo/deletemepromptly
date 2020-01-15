26e77682c50aaac6e80769e3cd9ab0f56d6c8036
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-01-15 14:02:06 +0100

    yes

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 require('dotenv').config()
 
+const helmet = require('helmet')
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 4000
 
 app.use(express.json())
+app.use(helmet())
 
 app.use((req, res) => {
   res.json('alive')
