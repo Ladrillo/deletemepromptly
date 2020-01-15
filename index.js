34c45c09c82990c7aee793934799d2d04bdc02b3
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-01-15 14:29:20 +0100

    reorder middlewares

--- index.js
+++ index.js
@@ -10,12 +10,12 @@ app.use(express.json())
 app.use(helmet())
 app.use(express.static(path.join(__dirname, 'client/build')))
 
-app.use((req, res) => {
-  res.json('alive')
-})
-
 app.get('/greeting', (req, res) => {
   res.json('hello')
+})
+
+app.use((req, res) => {
+  res.json('alive')
 })
 
 app.listen(port, () => {
